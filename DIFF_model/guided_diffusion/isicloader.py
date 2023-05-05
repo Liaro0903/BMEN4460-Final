14c8ec59bc405acee60e0022b1eb94c1e001d71a
Charlie Jiang <cj2630@columbia.edu>
2023-05-05 18:43:20 -0400

    Initial

--- DIFF_model/guided_diffusion/isicloader.py
+++ DIFF_model/guided_diffusion/isicloader.py
@@ -1,27 +1,34 @@
 import os
+import sys
+import pickle
 import glob
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import cv2
-from sklearn.metrics import confusion_matrix, accuracy_score
-
+from skimage import io
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.optim import Adam, SGD
-from torch.utils.data import TensorDataset, DataLoader
-
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms.functional as F
+import torchvision.transforms as transforms
+import pandas as pd
+from skimage.transform import rotate
 import nibabel as nib
 
 data_root_folder = '../data/'
-class BasicDataset(TensorDataset):
-    # This function takes folder name ('train', 'valid', 'test') as input and creates an instance of BasicDataset according to that fodler.
-    # Also if you'dd like to have less number of samples (for evaluation purposes), you may set the `n_sample` with an integer.
-    def __init__(self, folder, mode, n_sample=None):
-        ## Get images ids and the training images dirs
+class ISICDataset(Dataset):
+    def __init__(self, folder, mode ='train'):
+
+
+        #df = pd.read_csv(os.path.join(data_path, 'ISBI2016_ISIC_Part3B_' + mode + '_GroundTruth.csv'), encoding='gbk')
+
+        #self.name_list = df.iloc[:,1].tolist()
+        #self.label_list = df.iloc[:,2].tolist()
+        #self.data_path = data_path
+        #self.mode = mode
+
+        #self.transform = transform
+
         if (mode == 'train'):
             imgs_dir = os.path.join(data_root_folder, folder, 'Fold_2')
             img_dirs = sorted(glob.glob(os.path.join(imgs_dir, '*.gz')))
@@ -29,14 +36,14 @@ class BasicDataset(TensorDataset):
             img_dirs.extend(sorted(glob.glob(os.path.join(imgs_dir, '*.gz'))))
             imgs_dir = os.path.join(data_root_folder, folder, 'Fold_4')
             img_dirs.extend(sorted(glob.glob(os.path.join(imgs_dir, '*.gz'))))
+            print('Train DL')
         elif (mode == 'valid'):
             imgs_dir = os.path.join(data_root_folder, folder, 'Fold_5')
             img_dirs = sorted(glob.glob(os.path.join(imgs_dir, '*.gz')))
         elif (mode == 'test'):
             imgs_dir = os.path.join(data_root_folder, folder, 'Fold_1')
             img_dirs = sorted(glob.glob(os.path.join(imgs_dir, '*.gz')))
-        # print(len(img_dirs))
-        # convert to dictionary for easier access
+
         self.img_dirs_dic = [(img[img.index('00'):img.index('_session')], [img]) for img in img_dirs]
         # print(self.img_dirs_dic[0:3])
         self.img_dirs_dic = {k: v for k, v in self.img_dirs_dic}
@@ -55,29 +62,38 @@ class BasicDataset(TensorDataset):
             
         self.ids = list(self.img_dirs_dic.keys())
         self.n_sample = len(self.img_dirs_dic.keys())
-        
-        # If n_sample is not None (It has been set by the user)
-#         if not n_sample or n_sample > len(self.imgs_file):
-#             n_sample = len(self.imgs_file)
-        
-#         self.n_sample = n_sample
-#         self.ids = list([i+1 for i in range(n_sample)])
-            
-    # This function returns the lenght of the dataset (AKA number of samples in that set)
+
     def __len__(self):
         return self.n_sample
-    
-    
-    # This function takes an index (i) which is between 0 to `len(BasicDataset)` (The return of the previous function), then returns RGB image, 
-    # mask (Binary), and the index of the file name (Which we will use for visualization). The preprocessing step is also implemented in this function.
-    def __getitem__(self, i):
-        imgid = self.ids[i]
+
+    def __getitem__(self, index):
+        """Get the images"""
+        #name = self.name_list[index]
+        #img_path = os.path.join(self.data_path, name)
+        
+        #mask_name = self.label_list[index]
+        #msk_path = os.path.join(self.data_path, mask_name)
+
+        #img = Image.open(img_path).convert('RGB')
+        #mask = Image.open(msk_path).convert('L')
+
+        # if self.mode == 'Training':
+        #     label = 0 if self.label_list[index] == 'benign' else 1
+        # else:
+        #     label = int(self.label_list[index])
+
+        #if self.transform:
+        #    state = torch.get_rng_state()
+        #    img = self.transform(img)
+        #    torch.set_rng_state(state)
+        #    mask = self.transform(mask)
+        imgid = self.ids[index]
         
         # Read the actual image
         img = nib.load(self.img_dirs_dic[imgid][0]).get_fdata()
         mask0 = nib.load(self.img_dirs_dic[imgid][1]).get_fdata()
-        mask1 = nib.load(self.img_dirs_dic[imgid][2]).get_fdata()
-        mask2 = nib.load(self.img_dirs_dic[imgid][3]).get_fdata()
+        #mask1 = nib.load(self.img_dirs_dic[imgid][2]).get_fdata()
+        #mask2 = nib.load(self.img_dirs_dic[imgid][3]).get_fdata()
         
         # Resize images to (192, 192, 192)
         img = img[:,13:205,:]
@@ -86,12 +102,12 @@ class BasicDataset(TensorDataset):
         mask0 = mask0[:,13:205,:]
         mask0 = np.append(mask0, np.zeros((10, 192, 182)), axis=0)
         mask0 = np.append(mask0, np.zeros((192, 192, 10)), axis=2)
-        mask1 = mask1[:,13:205,:]
-        mask1 = np.append(mask1, np.zeros((10, 192, 182)), axis=0)
-        mask1 = np.append(mask1, np.zeros((192, 192, 10)), axis=2)
-        mask2 = mask2[:,13:205,:]
-        mask2 = np.append(mask2, np.zeros((10, 192, 182)), axis=0)
-        mask2 = np.append(mask2, np.zeros((192, 192, 10)), axis=2)
+        #mask1 = mask1[:,13:205,:]
+        #mask1 = np.append(mask1, np.zeros((10, 192, 182)), axis=0)
+        #mask1 = np.append(mask1, np.zeros((192, 192, 10)), axis=2)
+        #mask2 = mask2[:,13:205,:]
+        #mask2 = np.append(mask2, np.zeros((10, 192, 182)), axis=0)
+        #mask2 = np.append(mask2, np.zeros((192, 192, 10)), axis=2)
         
         # img = cv2.imread(os.path.join(self.imgs_dir, 'image_{0:04d}.png'.format(idx)), cv2.IMREAD_COLOR)
         # mask = cv2.imread(os.path.join(self.masks_dir, 'mask_{0:04d}.png'.format(idx)), cv2.IMREAD_GRAYSCALE)
@@ -107,28 +123,29 @@ class BasicDataset(TensorDataset):
         # Scale between 0 to 1
         img = np.array(img) / 255.0
         mask0 = np.array(mask0) / 255.0
-        mask1 = np.array(mask1) / 255.0
-        mask2 = np.array(mask2) / 255.0
+        #mask1 = np.array(mask1) / 255.0
+        #mask2 = np.array(mask2) / 255.0
         
         # Make sure that the mask are binary (0 or 1)
         mask0[mask0 <= 0.5] = 0.0
         mask0[mask0 > 0.5] = 1.0
-        mask1[mask1 <= 0.5] = 0.0
-        mask1[mask1 > 0.5] = 1.0
-        mask2[mask2 <= 0.5] = 0.0
-        mask2[mask2 > 0.5] = 1.0
+        #mask1[mask1 <= 0.5] = 0.0
+        #mask1[mask1 > 0.5] = 1.0
+        #mask2[mask2 <= 0.5] = 0.0
+        #mask2[mask2 > 0.5] = 1.0
         
         # Add an axis to the mask array so that it is in [channel, width, height] format.
         #mask = np.expand_dims(mask, axis=0)
-        img = img[np.newaxis,:]
-        mask = np.stack((mask0, mask1, mask2), axis=0)
+        img = img
+        mask = mask0
+        #mask = np.stack((mask0, mask1, mask2), axis=0)
         
         # HWC to CHW
         #img = np.transpose(img, (2, 0, 1))
         
 
-        return {
-          'image': torch.from_numpy(img).type(torch.FloatTensor),
-          'mask': torch.from_numpy(mask).type(torch.FloatTensor),
-          'img_id': imgid
-        }
+        return (torch.from_numpy(img).type(torch.FloatTensor),
+          torch.from_numpy(mask).type(torch.FloatTensor),
+          imgid)
+  
+        #return (img, mask, name)
